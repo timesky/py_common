5162e6729da080225541da6568beab74e593d169
tangming <tangming@huiyoukeji.net>
2024-08-28 17:31:09 +0800

    fix: 在日志记录时增加pid和taskname

--- extensions/logger_extras.py
+++ extensions/logger_extras.py
@@ -1,7 +1,22 @@
+import asyncio
 import contextvars
+import os
 
 # 定义一个全局的上下文变量，用于存储额外信息
 log_extra_unique_id = contextvars.ContextVar("log_extra_request_id", default=None)
+
+
+def get_uid_prefix():
+    uid_prefix = ''
+    try:
+        # 取得进程id
+        pid = os.getpid()
+        # 取得协程id
+        ctn = asyncio.current_task().get_name()
+        uid_prefix = f'{pid}-{ctn}'
+    except:
+        pass
+    return uid_prefix
 
 
 class MaskUniqueID:
@@ -11,6 +26,9 @@ class MaskUniqueID:
     """
 
     def set(self, value):
+        uid = get_uid_prefix()
+        if value:
+            uid = f'{uid}-{uid}'
         log_extra_unique_id.set(value)
 
     def __str__(self) -> str:
